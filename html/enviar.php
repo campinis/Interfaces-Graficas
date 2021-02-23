d07fbc39bf612025204cfa83dffa72fb18be7f11
campinis <79237467+campinis@users.noreply.github.com>
2021-02-22 23:57:14 -0600

    Add files via upload

--- html/enviar.php
+++ html/enviar.php
@@ -1,7 +1,6 @@
 <?php
-$name = $_POST['name'];
+$asunto = $_POST['asunto'];
 $mail = $_POST['mail'];
-$phone = $_POST['phone'];
 $message = $_POST['message'];
 
 $header = 'From: ' . $mail . " \r\n";
@@ -9,16 +8,14 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message = "Este mensaje fue enviado por: " . $asunto . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Teléfono de contacto: " . $phone . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
-$para = 'milenioflavio@hotmail.com';
-$asunto = 'Mensaje de PRECOMPC';
+$para = 'estebancampinis@hotmail.com';
 
 mail($para, $asunto, utf8_decode($message), $header);
 
-header("Location:index.html");
+header("Location:contacto.html");
 ?>
